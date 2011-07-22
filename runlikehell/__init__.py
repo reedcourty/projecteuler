2da7f51a439c2fbee089f0245545e1cc53d1ca3e
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-07-22 15:17:52 +0200

    Added runlikehell module for running time testing. Added .gitignore file (*.pyc)

---
+++ runlikehell/__init__.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import datetime
+
+class Stopper():
+    def __init__(self):
+        pass
+        
+    def start(self):
+        self.now = datetime.datetime.now()
+        
+    def stop(self):
+        self.result = datetime.datetime.now() - self.now
+        
+    def get_result(self):
+        return str(self.result.total_seconds())
