88f24aa8b5b48ac3824c0ff1d7b45402ba3783ae
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-07-22 15:40:08 +0200

    Added "Problem 1" and it's solution.

---
+++ problems/pe_001/pe_001.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Answer: 233168
+
+import runlikehell as rh
+
+LIMIT = 1000
+
+s = rh.Stopper()
+s.start()
+sum = 0
+for i in range(0, LIMIT):
+    if ((i % 3)==0) or ((i % 5)==0):
+        sum=sum+i
+print(sum)
+s.stop()
+
+print("running time: " + s.get_result()) 
+
+
